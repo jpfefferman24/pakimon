7eaaed71845856a5de3c059cd6a3f21ca5109c0c
jpfefferman24 <jonas.pfefferman.24@bishops.com>
2022-03-10 10:40:48 -0800

    Started work on classes

--- pakidex/models.py
+++ pakidex/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(user, on_delete=models.CASCADE, primary_key = True)
+    verified = models.BooleanField(default = False)
+
+class Deck(models.Model):
+    deck = models.OneToOneField(deck, on_delete=models.CASCADE, primary_key = True)
+    verified = models.BooleanField(default = False)
+
+class Card(models.Model):
+    pakiName = models.CharField(max_length = 280)
+    whoseDeck = models.ForeignKey(Deck, on_delete=models.CASCADE)
+    moves = []
+
+class Move(models.Model):
+    move = models.ForeignKey(
+        Card,
+        on_delete = models.CASCADE,
+        null = True,
+    )
+
+class Pakimon(models.Model):
+    paki = models.ForeignKey(
+        Card,
+        on_delete = models.CASCADE
+        null = True,
+    )
--- pakidex/views.py
+++ pakidex/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
