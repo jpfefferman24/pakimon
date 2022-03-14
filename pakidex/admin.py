ecf13bccba9842b8d937b1ee634311abd5717abb
jpfefferman24 <jonas.pfefferman.24@bishops.com>
2022-03-14 09:30:17 -0700

    Admin is up and running. Fix Pakimon & Move save()

--- pakidex/admin.py
+++ pakidex/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Profile, Deck, Card, Move, Pakimon
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Deck)
+admin.site.register(Card)
+admin.site.register(Move)
+admin.site.register(Pakimon)
--- pakidex/models.py
+++ pakidex/models.py
@@ -4,28 +4,38 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
-    user = models.OneToOneField(user, on_delete=models.CASCADE, primary_key = True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key = True)
     verified = models.BooleanField(default = False)
 
 class Deck(models.Model):
-    deck = models.OneToOneField(deck, on_delete=models.CASCADE, primary_key = True)
+    deck = models.OneToOneField(User, on_delete=models.CASCADE, primary_key = True)
     verified = models.BooleanField(default = False)
 
 class Card(models.Model):
-    pakiName = models.CharField(max_length = 280)
+    personalName = models.CharField(max_length = 280)
     whoseDeck = models.ForeignKey(Deck, on_delete=models.CASCADE)
-    moves = []
 
 class Move(models.Model):
-    move = models.ForeignKey(
+    whichCard = models.ForeignKey(
         Card,
         on_delete = models.CASCADE,
         null = True,
     )
+    moveName = models.CharField(max_length = 280)
+    moveNum = models.IntegerField(default = -1)
+
+    def __str__(self):
+        moveStr = "Move Name: " + moveName + ", Move Number: " + moveNum
+        return moveStr
 
 class Pakimon(models.Model):
-    paki = models.ForeignKey(
+    whichCard = models.ForeignKey(
         Card,
-        on_delete = models.CASCADE
+        on_delete = models.CASCADE,
         null = True,
     )
+    pakiName = models.CharField(max_length = 280)
+
+    def __str__(self):
+        pakiStr = "Pakimon Name: " + pakiName
+        return pakiStr
